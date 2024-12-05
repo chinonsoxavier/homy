cd67eba7483aa62240546014ccba4734b048229e
chinonsoxavier <chinonsoxavier26@gmail.com>
2024-12-05 09:46:00 +0100

    first commit

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeMainView from "../views/app/home/HomeMainView.vue";
 import { agentRoutes } from "./agent";
 import ListingsMainView from "../views/app/listings/ListingsMainView.vue";
-import { useUserStore } from "../utilities/store";
+// import { useUserStore } from "../utilities/store";
 
 const routes = [
   ...agentRoutes,
@@ -38,17 +38,17 @@ const router = createRouter({
   routes: routes,
 });
 
-router.beforeEach((to, from, next) => {
-  const userStore = useUserStore();
-  console.log(from);
-  const role = userStore.user?.role;
-  // const isLoggedIn = userStore.user;
-  if (to.meta.requiresAuth && role != "agent") {
-    next({ name: "home" });
-    return;
-  } else {
-    next();
-  }
-});
+// router.beforeEach((to, from, next) => {
+//   const userStore = useUserStore();
+//   console.log(from);
+//   const role = userStore.user?.role;
+//   // const isLoggedIn = userStore.user;
+//   if (to.meta.requiresAuth && role != "agent") {
+//     next({ name: "home" });
+//     return;
+//   } else {
+//     next();
+//   }
+// });
 
 export default router;
